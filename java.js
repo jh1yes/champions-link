d2dccc451c267592ddc62aa1a80517b98ffe593e
jh1yes <jh1yes@hotmail.com>
2024-03-27 23:58:06 -0300

    postagem1
    link dos campeões

---
+++ java.js
@@ -0,0 +1,49 @@
+const box = document.querySelector(".container");
+const imagens = document.querySelectorAll(".container img");
+
+let contador = 0;
+
+function slider() {
+    contador++;
+
+    if(contador > imagens.length - 1) {
+        contador = 0;
+    }
+    box.style.transform = `translateX(${-contador * 380}px)`;
+}
+
+setInterval(slider, 2000);
+
+
+
+
+function mudafoto (foto)
+{
+document.getElementById("simbolo").src = foto;
+}
+
+
+
+const box2 = document.querySelector(".container2");
+const imagens2 = document.querySelectorAll(".container2 img");
+
+let contador2 = 0;
+
+function slider2() {
+    contador2++;
+
+    if(contador2 > imagens.length - 1) {
+        contador2 = 0;
+    }
+    box2.style.transform = `translateX(${-contador2 * 890}px)`;
+}
+
+setInterval(slider2, 2000);
+
+
+
+
+function mudafoto (foto2)
+{
+document.getElementById("simbolo").src = foto2;
+}
